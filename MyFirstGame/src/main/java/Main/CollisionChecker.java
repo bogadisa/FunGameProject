4616146a3e3b83ac32881965affc67fddde7fa91
Magnus Hoddevik <bjornhod@student.matnat.uio.no>
2025-02-04 16:24:36 +0100

    Made it easier to check collision in a specific direction

--- MyFirstGame/src/main/java/Main/CollisionChecker.java
+++ MyFirstGame/src/main/java/Main/CollisionChecker.java
@@ -40,6 +40,90 @@ public class CollisionChecker {
         return false;
     }
 
+    public boolean checkRowCollision(Entity entity, int x, int y, int topCornerCol, int topCornerRow, int botCornerCol,
+            int botCornerRow) {
+        Rectangle solidArea = entity.getSolidRectangle();
+        int entityWidth = solidArea.width;
+        boolean collision = false;
+        if (entity.speedX < 0) {
+            int distToNextCol = (topCornerCol) * gp.tileSize - x;
+            if (entity.speedX <= distToNextCol) {
+                collision = checkTile(topCornerCol - 1, topCornerRow) || checkTile(topCornerCol - 1, botCornerRow);
+                if (collision) {
+                    // stops entity from entering the next tile
+                    entity.speedX = distToNextCol + 1;
+                }
+            }
+
+        } else if (entity.speedX > 0) {
+            int distToNextCol = (botCornerCol + 1) * gp.tileSize - (x + entityWidth);
+
+            if (entity.speedX >= distToNextCol) {
+                collision = checkTile(botCornerCol + 1, botCornerRow) || checkTile(botCornerCol + 1, topCornerRow);
+                if (collision) {
+                    entity.speedX = distToNextCol - 1;
+                }
+            }
+        }
+        return collision;
+    }
+
+    public boolean checkRowCollision(Entity entity) {
+        Rectangle solidArea = entity.getSolidRectangle();
+        int x = solidArea.x;
+        int entityWidth = solidArea.width;
+        int y = solidArea.y;
+        int entityHeight = solidArea.height;
+
+        int topCornerCol = (int) (x / gp.tileSize);
+        int topCornerRow = (int) (y / gp.tileSize);
+        int botCornerCol = (int) ((x + entityWidth) / gp.tileSize);
+        int botCornerRow = (int) ((y + entityHeight) / gp.tileSize);
+
+        return checkRowCollision(entity, x, y, topCornerCol, topCornerRow, botCornerCol, botCornerRow);
+    }
+
+    public boolean checkColCollision(Entity entity, int x, int y, int topCornerCol, int topCornerRow, int botCornerCol,
+            int botCornerRow) {
+        Rectangle solidArea = entity.getSolidRectangle();
+        int entityHeight = solidArea.height;
+        boolean collision = false;
+        if (entity.speedY < 0) {
+            int distToNextRow = (topCornerRow) * gp.tileSize - y;
+            if (entity.speedY <= distToNextRow) {
+                collision = checkTile(topCornerCol, topCornerRow - 1) || checkTile(botCornerCol, topCornerRow - 1);
+                if (collision) {
+                    entity.speedY = distToNextRow + 1;
+                }
+            }
+
+        } else if (entity.speedY > 0) {
+            int distToNextRow = (botCornerRow + 1) * gp.tileSize - (y + entityHeight);
+            if (entity.speedY >= distToNextRow) {
+                collision = checkTile(botCornerCol, botCornerRow + 1) || checkTile(topCornerCol, botCornerRow + 1);
+                if (collision) {
+                    entity.speedY = distToNextRow - 1;
+                }
+            }
+        }
+        return collision;
+    }
+
+    public boolean checkColCollision(Entity entity) {
+        Rectangle solidArea = entity.getSolidRectangle();
+        int x = solidArea.x;
+        int entityWidth = solidArea.width;
+        int y = solidArea.y;
+        int entityHeight = solidArea.height;
+
+        int topCornerCol = (int) (x / gp.tileSize);
+        int topCornerRow = (int) (y / gp.tileSize);
+        int botCornerCol = (int) ((x + entityWidth) / gp.tileSize);
+        int botCornerRow = (int) ((y + entityHeight) / gp.tileSize);
+
+        return checkColCollision(entity, x, y, topCornerCol, topCornerRow, botCornerCol, botCornerRow);
+    }
+
     public void checkCollision(Entity entity) {
         Rectangle solidArea = entity.getSolidRectangle();
         int x = solidArea.x;
@@ -52,45 +136,7 @@ public class CollisionChecker {
         int botCornerCol = (int) ((x + entityWidth) / gp.tileSize);
         int botCornerRow = (int) ((y + entityHeight) / gp.tileSize);
 
-        boolean collisionX = false;
-        if (entity.speedX < 0) {
-            int distToNextCol = (topCornerCol) * gp.tileSize - x;
-            if (entity.speedX <= distToNextCol) {
-                collisionX = checkTile(topCornerCol - 1, topCornerRow) || checkTile(topCornerCol - 1, botCornerRow);
-                if (collisionX) {
-                    // stops entity from entering the next tile
-                    entity.speedX = distToNextCol + 1;
-                }
-            }
-
-        } else if (entity.speedX > 0) {
-            int distToNextCol = (botCornerCol + 1) * gp.tileSize - (x + entityWidth);
-
-            if (entity.speedX >= distToNextCol) {
-                collisionX = checkTile(botCornerCol + 1, botCornerRow) || checkTile(botCornerCol + 1, topCornerRow);
-                if (collisionX) {
-                    entity.speedX = distToNextCol - 1;
-                }
-            }
-        }
-        boolean collisionY;
-        if (entity.speedY < 0) {
-            int distToNextRow = (topCornerRow) * gp.tileSize - y;
-            if (entity.speedY <= distToNextRow) {
-                collisionY = checkTile(topCornerCol, topCornerRow - 1) || checkTile(botCornerCol, topCornerRow - 1);
-                if (collisionY) {
-                    entity.speedY = distToNextRow + 1;
-                }
-            }
-
-        } else if (entity.speedY > 0) {
-            int distToNextRow = (botCornerRow + 1) * gp.tileSize - (y + entityHeight);
-            if (entity.speedY >= distToNextRow) {
-                collisionY = checkTile(botCornerCol, botCornerRow + 1) || checkTile(topCornerCol, botCornerRow + 1);
-                if (collisionY) {
-                    entity.speedY = distToNextRow - 1;
-                }
-            }
-        }
+        checkRowCollision(entity, x, y, topCornerCol, topCornerRow, botCornerCol, botCornerRow);
+        checkColCollision(entity, x, y, topCornerCol, topCornerRow, botCornerCol, botCornerRow);
     }
 }
