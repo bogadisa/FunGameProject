63fc6bd268707f312dcef962cdc854d0502ad07b
Magnus Hoddevik <bjornhod@student.matnat.uio.no>
2025-04-25 08:46:20 +0200

    Fixed the sprite sheet loading

--- MyFirstGame/src/main/java/secondEngine/components/SpriteSheet.java
+++ MyFirstGame/src/main/java/secondEngine/components/SpriteSheet.java
@@ -20,8 +20,8 @@ public class SpriteSheet {
 
         String metadata[] = filepath.substring(0, filepath.length() - 4).split("_");
 
-        int spriteWidth = Integer.parseInt(metadata[1]);
-        int spriteHeight = Integer.parseInt(metadata[2]);
+        int spriteWidth = this.texture.getWidth() / Integer.parseInt(metadata[1]);
+        int spriteHeight = this.texture.getHeight() / Integer.parseInt(metadata[2]);
         int numSprites = Integer.parseInt(metadata[3]);
 
         loadSpriteSheet(this.texture, spriteWidth, spriteHeight, numSprites, 0);
@@ -31,31 +31,28 @@ public class SpriteSheet {
         // System.out.println(spriteWidth + "" + spriteHeight + "" + numSprites);
         // System.exit(0);
 
-        int i = 0;
         int currentX = 0;
-        int currentY = 0;
-        while (i < numSprites) {
-            System.out.println(i);
-            float topY = currentY + spriteHeight;
-            float rightX = currentX + spriteWidth;
-            float leftX = currentX;
-            float bottomY = currentY;
+        int currentY = texture.getHeight() - spriteHeight;
+        for (int i = 0; i < numSprites; i++) {
+            float topY = (currentY + spriteHeight) / (float) texture.getHeight();
+            float rightX = (currentX + spriteWidth) / (float) texture.getWidth();
+            float leftX = currentX / (float) texture.getHeight();
+            float bottomY = currentY / (float) texture.getWidth();
             // TODO potential source of wrong image orientation
             Vector2f currentTexCoords[] = {
                     new Vector2f(rightX, topY),
-                    new Vector2f(leftX, bottomY),
                     new Vector2f(rightX, bottomY),
+                    new Vector2f(leftX, bottomY),
                     new Vector2f(leftX, topY)
             };
 
             Sprite sprite = new Sprite(this.texture, currentTexCoords);
             this.sprites.add(sprite);
-            i++;
 
             currentX += spriteWidth + spacing;
             if (currentX >= texture.getWidth()) {
                 currentX = 0;
-                currentY += spriteHeight + spacing;
+                currentY -= spriteHeight + spacing;
             }
         }
     }
